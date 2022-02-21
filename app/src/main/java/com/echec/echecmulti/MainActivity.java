b4b44b2278d1d1842a4d67e2a8f1d3b98124de0e
Hugo <78080239+mordeur@users.noreply.github.com>
2022-02-21 12:37:44 +0100

    Initial commit

---
+++ app/src/main/java/com/echec/echecmulti/MainActivity.java
@@ -0,0 +1,82 @@
+package com.echec.echecmulti;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    Button button;
+    String playerName="";
+    FirebaseDatabase database;
+    DatabaseReference playerRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //regarder si le joueur existe
+        editText = findViewById(R.id.editTextLogin);
+        button = findViewById(R.id.buttonLogin);
+
+        database = FirebaseDatabase.getInstance();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playerName = editText.getText().toString();
+                editText.setText("");
+                if(!playerName.equals("")){
+                    button.setText("connections en cours ...");
+                    button.setEnabled(false);
+                    playerRef = database.getReference("players/" + playerName);
+                    addEventListener();
+                    playerRef.setValue("");
+
+                }
+            }
+        });
+
+    }
+    public void addEventListener(){
+        playerRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                //succer - continue sur le prochain écran et sauvegarde le nom du joueur
+                if (!playerName.equals("")){
+                    //Toast.makeText(MainActivity.this,"sa a changer",Toast.LENGTH_SHORT).show();
+                    SharedPreferences preferences = getSharedPreferences("PREFS",0);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("playerName", playerName);
+                    editor.apply();
+                    button.setText("connections");
+                    button.setEnabled(true);
+                    //startActivity(new Intent(getApplicationContext(),RoomActivity.class));
+                    finish();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                //erreur
+                button.setText("connections");
+                button.setEnabled(true);
+                Toast.makeText(MainActivity.this,"erreur!",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
