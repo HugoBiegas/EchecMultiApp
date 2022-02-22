b6704d54e91f2c00ea8acc75a33d30090f541e3c
Vincent <vincenttvannn@gmail.com>
2022-02-22 15:07:15 +0100

    [Firebase, Layouts, Class]Système d'authentification

--- app/src/main/java/com/echec/echecmulti/MainActivity.java
+++ app/src/main/java/com/echec/echecmulti/MainActivity.java
@@ -6,85 +6,152 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
-    EditText editText; //nom de l'utilisateur
-    Button button;  //bouton de connections
-    String playerName="";   //le nom du joueur
-    FirebaseDatabase database; // connections as la BDD
-    DatabaseReference playerRef;// référence de la BD
+    ListView listView;//liste des View
+    Button button;//bouton de créations de room
+    List<String> roomList = new ArrayList<>();//liste des room
+    String playerName="";//nom du joueur
+    String roomName="";//nom de la room
+    FirebaseDatabase database;//connections a la base de données
+    DatabaseReference roomRef;//référence as la base de donnée pour une room
+    DatabaseReference roomsRef;//référence as la base de donnée pour les room
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_room);
         initialisation();
-        BTNClique();
-
-    }
-    private void initialisation(){
-        //regarder si le joueur existe
-        editText = findViewById(R.id.editTextLogin);//récupérations du champs nom
-        button = findViewById(R.id.buttonLogin);//récupérations du bouton(pour l'actions)
-        database = FirebaseDatabase.getInstance();//créations de l'instance
-    }
-    private void BTNClique(){
-        //action appret avoir cliquer sur connection
-        button.setOnClickListener(btntchec());
+        CréationRoom();
+        ItemCliquer();
+        addRoomsEventListener();
     }
 
-    private View.OnClickListener btntchec(){
-        return new View.OnClickListener() {
+    private void ItemCliquer(){
+        //si une personne clique sur un itéme de la liste des room
+        listView.setOnItemClickListener(Itemcréjoueur2());
+    }
+
+    private AdapterView.OnItemClickListener Itemcréjoueur2(){
+        return new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                playerName = editText.getText().toString();//récupérations du nom de l'utilisateur
-                editText.setText("");//remise as "" du texte
-                if(!playerName.equals("")){//on vérifie bien que l'utilisateur as sési un truque
-                    button.setText("connections en cours ...");//changement du bouton connections en connections en cours
-                    button.setEnabled(false);//pour que le bouton ne soit pas clicable une dexiéme foi
-                    playerRef = database.getReference("players/" + playerName);//créations de players avec une sous catégori avec le nom du joueur
-                    addEventListener();//appelle de fonctions
-                    playerRef.setValue("");//on mais la référence du joueur a ""(on pourat mettre d'autre info)
-
-                }
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //créations du joueur 2
+                roomName = roomList.get(i);//on donne as roomName le nom de l'endroi ou a cliquer la personne
+                roomRef = database.getReference("rooms/" + roomName + "/player2");//on crée le player2 dans la partie
+                addRoomEventListener();//appelle de l'actions pour changer de page
+                roomRef.setValue(playerName);//on mais la valeur de player2 aux nom du joueur
             }
         };
     }
-    //créations de l'endroit ou on vas gérer les évenement
-    private void addEventListener(){
-        playerRef.addValueEventListener(addEnvent());
+
+    private void CréationRoom(){
+        //si le bouton de créations de room est cliquer
+        button.setOnClickListener(cliqueCrée());
     }
-    private ValueEventListener addEnvent(){
+
+    private View.OnClickListener cliqueCrée(){
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //créations de la room et assigniations du joueur 1
+                button.setText("créations de room");
+                button.setEnabled(false);
+                finish();
+                startActivity(new Intent(getApplicationContext(),CreaRoom.class));//on lance l'activiter RooomActivity
+
+            }
+        };
+    }
+
+    private void initialisation(){
+        database = FirebaseDatabase.getInstance();
+        //récupérer le nom du joueur et donner le nom du joueur pour la room
+        SharedPreferences preferences = getSharedPreferences("PREFS",0);
+        playerName = preferences.getString("playerName","");
+        listView = findViewById(R.id.listRoom);//affectations de la liste des room
+        button = findViewById(R.id.buttonCreateRoom);//affectations du bonton
+    }
+
+    private void addRoomEventListener(){
+        roomRef.addValueEventListener(addRoomEvent());
+    }
+
+    private ValueEventListener addRoomEvent(){
         return new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //rejoidre une room
+                button.setText("crations de room");//changement du bouton
+                button.setEnabled(true);//changement du bouton pour le rendre non-cliquable
+                Intent intent = new Intent(getApplicationContext(), GameActivity.class);//créations de la page Game
+                intent.putExtra("roomName",roomName);//on donne en extrat la valeur de la roomName pour savoir si la personne et un gest ou l'host
+                intent.putExtra("playerhost","");//on donne en extrat la valeur de la roomName pour savoir si la personne et un gest ou l'host
+                startActivity(intent);//on lance l'activiter
 
-                //succer - continue sur le prochain écran et sauvegarde le nom du joueur
-                SharedPreferences preferences = getSharedPreferences("PREFS",0);//permet de modifier un ensemble particulier
-                SharedPreferences.Editor editor = preferences.edit();//permet de changer la valeur contenut dans preferences
-                editor.putString("playerName", playerName);//remplace le nom du joueur par le vraix nom du joueur
-                editor.apply();//applique le changement de nom de joueur
-                startActivity(new Intent(getApplicationContext(),RoomActivity.class));//on lance l'activiter RooomActivity
-                finish();//on arréte la tache actuelle
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                //erreur
-                button.setText("connections");//change le texte du bouton
-                button.setEnabled(true);//rend visible le bouton
-                Toast.makeText(MainActivity.this,"erreur!",Toast.LENGTH_SHORT).show();//affiche un message d'erreur
+                //ereur
+                button.setText("creations de room");//changement du bouton
+                button.setEnabled(true);//changement du bouton pour le rendre non-cliquable
+                Toast.makeText(MainActivity.this,"erreur!",Toast.LENGTH_SHORT).show();//message d'erreur
+
             }
         };
+    }
+
+    private void addRoomsEventListener(){
+        roomsRef = database.getReference("rooms");//on récupére la référence de rooms
+        roomsRef.addValueEventListener(addRoomsEvent());
+    }
+    private ValueEventListener addRoomsEvent(){
+        return new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //controle de la liste des room
+                roomList.clear();//on enléve de la vue de l'utilisteur tout les rooms
+                Iterable<DataSnapshot> rooms = snapshot.getChildren();// on prend l'état des rooms as un moment donner
+                for (DataSnapshot snapshot1 : rooms){// on vas regarder tout les room existante
+                    if(!snapshot1.getValue().toString().contains("player2")){//verifie si il y as deux joueur
+                        roomList.add(snapshot1.getKey());//on vas rajouter les room une par une dans la liste
+                        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1,roomList);//créer une room dans roomActivity avec la liste roomList
+                        listView.setAdapter(adapter);//ajoute un élément dans la liste
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                //erreur rien
+            }
+        };
+    }
+
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(),Login.class));
+        finish();
     }
 }
--- app/src/main/java/com/echec/echecmulti/RoomActivity.java
+++ app/src/main/java/com/echec/echecmulti/RoomActivity.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -146,5 +147,11 @@ public class RoomActivity extends AppCompatActivity {
                 //erreur rien
             }
         };
+    }
+
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(),Login.class));
+        finish();
     }
 }
