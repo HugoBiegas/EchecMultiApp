6f81c6bac2db335f6c047d7aee280807f5b5ddaa
Hugo <78080239+mordeur@users.noreply.github.com>
2022-02-23 16:15:28 +0100

    tout les mouvements des petite pion et des déplacements

--- app/src/main/java/com/echec/echecmulti/GameActivity.java
+++ app/src/main/java/com/echec/echecmulti/GameActivity.java
@@ -12,7 +12,9 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -22,14 +24,23 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class GameActivity extends AppCompatActivity {
 
     GridView gridView;
+    String[] ColorAction = new String[32];
+    ArrayList<Integer> PositionValble =new ArrayList<>();
+    PetitePion pion = new PetitePion();
     int selectionner=-1;
     int coup=0;
+    int couleur=0;
+    int debugPiece=0;
+    TextView textView;
     Integer positiondépart=0;
     Integer positionarriver=0;
-    String[] number = new String[64];
+    String[] BordPiece = new String[64];
+    String[] colorP = new String[64];
     String playerName = "";
     String CompartPlayer="";
     String roomName = "";
@@ -74,45 +85,44 @@ public class GameActivity extends AppCompatActivity {
 
     private void initialistions(){
         gridView = findViewById(R.id.grid_echec);
-        gridView.setEnabled(false);
-
-        database = FirebaseDatabase.getInstance();//créer une instance
         buttonqui = findViewById(R.id.quiter);//récupére le bouton quiter
         gridView = findViewById(R.id.grid_echec);
+        textView = findViewById(R.id.NomJoueur);
+
+        gridView.setEnabled(false);
+        database = FirebaseDatabase.getInstance();//créer une instance
+
         SharedPreferences preferences = getSharedPreferences("PREFS",0);
         playerName = preferences.getString("playerName","");
+        textView.setText("joueur : " + playerName);
+
     }
     private void echecini(){
         initialisationsEchiquier();
         piécedéplacement();
-
     }
 
     private void déplacement(){
-        String déplace = number[positiondépart];
-        number[positiondépart]="";
-        number[positionarriver]=déplace;
+        String déplace = BordPiece[positiondépart];
+        BordPiece[positiondépart]="";
+        BordPiece[positionarriver]=déplace;
+        déplace = colorP[positiondépart];
+        colorP[positiondépart] ="";
+        colorP[positionarriver]= déplace;
         piécedéplacement();
 
     }
 
     private void piécedéplacement(){
+        //permet de mettre la couleur et de remettre le tableaux
         ArrayAdapter<String> adapter =
-                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, number) {
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, BordPiece) {
                     @Override
                     public View getView(int position, View convertView, ViewGroup parent) {
                         View view = super.getView(position, convertView, parent);
                         int color;
-                        int couleurNb=0;
-                        int[] positionColor = new int[]{1, 3, 5, 7, 8, 10, 12, 14, 17, 19, 21, 23, 24, 26, 28, 30, 33, 35, 37, 39, 40, 42, 44, 46, 49, 51, 53, 55, 56, 58, 60, 62};
-                        for(int i=0;i<32;i++){
-                            if(position == positionColor[i])
-                                couleurNb =1;
-                        }
-                        if (couleurNb == 1)
-                            color = Color.DKGRAY; // noir
-                        else
-                            color = Color.LTGRAY; // blanc
+                        couleur=0;
+                        color = coloration(position);
                         view.setBackgroundColor(color);
                         //couleur
                         return view;
@@ -122,21 +132,54 @@ public class GameActivity extends AppCompatActivity {
     }
 
     private void initialisationsEchiquier(){
-        for(int i = 0 ; i<number.length;i++){//pour intégrer tout les case
-            if(i==0||i==7||i==56||i==63)//tour
-                number[i]="T";
-            else if (i==1||i==6||i==57||i==62)//cavalier
-                number[i]="C";
-            else if (i==2||i==5||i==58||i==61)//foue
-                number[i]="F";
-            else if(i==3||i==59)//dame
-                number[i]="D";
-            else if(i==4||i==60)//roie
-                number[i]="R";
-            else if(8<=i && i<=15 || 48<=i && i<=55)//petite pion
-                number[i]="P";
-            else
-                number[i]="";
+        for(int i = 0 ; i<BordPiece.length;i++){//pour intégrer tout les case
+            if(i==0||i==7){
+                BordPiece[i]="T"; //tour
+                colorP[i] = "B";
+            }else if(i==56||i==63){
+                BordPiece[i]="T"; //tour
+                colorP[i] = "N";
+            }
+            else if (i==1||i==6){
+                BordPiece[i]="C";//cavalier
+                colorP[i] = "B";
+            }else if (i==57||i==62){
+                BordPiece[i]="C";//cavalier
+                colorP[i] = "N";
+            }
+            else if (i==2||i==5){
+                BordPiece[i]="F";//foue
+                colorP[i] = "B";
+            }else if (i==58||i==61){
+                BordPiece[i]="F";//foue
+                colorP[i] = "N";
+            }
+            else if(i==3){
+                BordPiece[i]="D"; //dame
+                colorP[i] = "B";
+            }else if (i==59){
+                BordPiece[i]="D"; //dame
+                colorP[i] = "N";
+            }
+            else if(i==4){
+                BordPiece[i]="R";//roie
+                colorP[i] = "B";
+            }else if (i==60){
+                BordPiece[i]="R";//roie
+                colorP[i] = "N";
+            }
+            else if(8<=i && i<=15){
+                BordPiece[i]="P";//petite pion
+                colorP[i] = "B";
+            }else if (48<=i && i<=55){
+                BordPiece[i]="P";//petite pion
+                colorP[i] = "N";
+            }
+            else{
+                BordPiece[i]="";
+                colorP[i] = "";
+            }
+
         }
     }
 
@@ -147,29 +190,154 @@ public class GameActivity extends AppCompatActivity {
         return new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                if (coup == 1 && selectionner == i){
-                    Toast.makeText(GameActivity.this, "veuiller selectionner une case valide", Toast.LENGTH_SHORT).show();
-                    coup--;
-                    selectionner=-1;
-                }
-                else if(coup == 0){
-                        coup++;
-                        selectionner = i;
-                    }else if(selectionner != i && coup == 1){
-                            positiondépart=selectionner;
-                            positionarriver=i;
-                            déplacement();
-                            gridView.setEnabled(false);
-                            messageRef.setValue(role+":"+selectionner+":"+i);//change l'informatiosn dans la BDD
-                            coup=0;
-                            selectionner = -1;
-                            Toast.makeText(GameActivity.this, "a votre adversaire de jouer", Toast.LENGTH_SHORT).show();
+                //on regarde si ces l'host et que la couleur est Blanc alors on mais le coup a 1 et selecltionner a i pui si coup == 1 alors on fait des test pour bouger
+                int posible=0;
+                if(role.equals("host") && colorP[i].equals("B") || coup==1){
+                    if(coup==1){//si la piéce et vide ou que ces le deusiéme coup
+                        if (selectionner == i){
+                            Toast.makeText(GameActivity.this, "veuiller selectionner une case valide", Toast.LENGTH_SHORT).show();
+                            coup--;
+                            selectionner=-1;
+                            piécedéplacement();
                         }
+                        else{
+                            trouverPositionCorecte(i);
+                        }
+                    }else
+                        trouverLesDéplacement(i);
+                }else if(role.equals("guest") && colorP[i].equals("N") || coup==1){
+                    if(coup==1){//si la piéce et vide ou que ces le deusiéme coup
+                        if (selectionner == i){
+                            Toast.makeText(GameActivity.this, "veuiller selectionner une case valide", Toast.LENGTH_SHORT).show();
+                            coup--;
+                            selectionner=-1;
+                            piécedéplacement();
+                        }
+                        else{
+                            trouverPositionCorecte(i);
+                            //trouverPositionCorecte(i);
+                        }
+
+                    }else
+                        trouverLesDéplacement(i);
+                }
             }
         };
     }
+    private void trouverPositionCorecte(int i){
+        int posible=0;
+        if (BordPiece[selectionner].equals("P")) {
+            for (int j = 0; j < PositionValble.size(); j++) {
+                if (i == PositionValble.get(j)) {
+                    posible = 1;
+                }
+            }
+            PositionValble.clear();
+            if (posible == 1) {
+                positiondépart = selectionner;
+                positionarriver = i;
+                déplacement();
+                gridView.setEnabled(false);
+                messageRef.setValue(role + ":" + selectionner + ":" + i);//change l'informatiosn dans la BDD
+                coup = 0;
+                selectionner = -1;
+                Toast.makeText(GameActivity.this, "a votre adversaire de jouer", Toast.LENGTH_SHORT).show();
+            } else {
+                positiondépart = -1;
+                coup--;
+                piécedéplacement();
+                Toast.makeText(GameActivity.this, "imposible", Toast.LENGTH_SHORT).show();
+            }
+        }else if (BordPiece[i].equals("T")){
+        }else if(BordPiece[i].equals("C")){
 
-    private void extragerer(){
+        }else if (BordPiece[i].equals("F")){
+
+        }else if (BordPiece[i].equals("D")){
+
+        }else if (BordPiece[i].equals("R")){
+
+        }else{
+            positiondépart = selectionner;
+            positionarriver = i;
+            déplacement();
+            gridView.setEnabled(false);
+            messageRef.setValue(role + ":" + selectionner + ":" + i);//change l'informatiosn dans la BDD
+            coup = 0;
+            selectionner = -1;
+            Toast.makeText(GameActivity.this, "a votre adversaire de jouer", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    private void trouverLesDéplacement(int i){
+        if (BordPiece[i].equals("P")){
+            if (role.equals("host"))
+                ColorAction = pion.déplacementHost(BordPiece,i,colorP);
+            else
+                ColorAction = pion.déplacementGuest(BordPiece,i,colorP);
+
+            piéceColorDeplacement();
+            debugPiece=0;
+        }
+        coup++;
+        selectionner = i;
+    }
+
+    private void piéceColorDeplacement() {
+        //permet de mettre la couleur et de remettre le tableaux
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, BordPiece) {
+                    @Override
+                    public View getView(int position, View convertView, ViewGroup parent) {
+                        View view = super.getView(position, convertView, parent);
+                        int color,couleurNb=0;
+                        debugPiece = 0;
+                        couleur=0;
+                        color = coloration(position);
+                        String[] Lettre = new String[4];
+                        Integer[] coordonner = new Integer[4];
+                        for (int i=0;i<4;i++){
+                            coordonner[i] = Integer.parseInt(ColorAction[i].substring(ColorAction[i].indexOf(":")+1,ColorAction[i].length()));
+                            Lettre[i] = ColorAction[i].substring(0,ColorAction[i].indexOf(":"));
+                            if (coordonner[i] == position){
+                                 if (Lettre[i].equals("A")){
+                                    couleurNb = 3;
+                                    PositionValble.add(coordonner[i]);
+                                }
+                                else if (Lettre[i].equals("D")){
+                                    couleurNb = 2;
+                                    PositionValble.add(coordonner[i]);
+                                }
+                            }
+                        }
+                        if(couleurNb == 2 )
+                            color = Color.GREEN;
+                        else if(couleurNb == 3)
+                            color = Color.RED;
+                        view.setBackgroundColor(color);
+                        return view;
+                    }
+                };
+        gridView.setAdapter(adapter);
+    }
+
+    private int coloration(int position){
+        int color;
+
+        int[] positionColor = new int[]{1, 3, 5, 7, 8, 10, 12, 14, 17, 19, 21, 23, 24, 26, 28, 30, 33, 35, 37, 39, 40, 42, 44, 46, 49, 51, 53, 55, 56, 58, 60, 62};
+        for(int i=0;i<32;i++){
+            if(position == positionColor[i])
+                couleur =1;
+        }
+        if (couleur == 1)
+            color = Color.DKGRAY; // noir
+        else
+            color = Color.LTGRAY; // blanc
+        return color;
+    }
+
+        private void extragerer(){
         Bundle extra = getIntent().getExtras();//récuper l'extrat envoiller par roomActivity
         if(extra != null){
             roomName = extra.getString("roomName");;//récupére la valeur envoiller
---
+++ app/src/main/java/com/echec/echecmulti/PetitePion.java
@@ -0,0 +1,83 @@
+package com.echec.echecmulti;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+public class PetitePion {
+
+    public String[] déplacementHost(String[] Echiquier,int coordoner,String[] color){
+        String[] déplacement = new String[4];
+        int cpt=0,premier=0;
+        //test tout les endroi ou peux marcher un pion et ou il peux manger
+        coordoner +=7;
+            for (int i=0;i<3;i++){
+                if (i == 0 || i==2){
+                    if(!Echiquier[coordoner].equals("") && color[coordoner].equals("N")){
+                        déplacement[cpt] = "A:"+coordoner;
+                        cpt++;
+                    }else{
+                        déplacement[cpt] = "R:"+coordoner;
+                        cpt++;
+                    }
+                }else{
+                    if (Echiquier[coordoner].equals("")){
+                        déplacement[cpt] = "D:"+coordoner;
+                        cpt++;
+                        premier =1;
+                    }else{
+                        déplacement[cpt] = "O:"+coordoner;
+                        cpt++;
+                        premier =0;
+                    }
+                }
+                coordoner++;
+            }
+        coordoner +=6;
+        if(Echiquier[coordoner].equals("") && premier == 1){
+            déplacement[cpt] = "D:"+coordoner;
+        }else{
+            déplacement[cpt] = "O:"+coordoner;
+        }
+        cpt=0;
+        return déplacement;
+    }
+    public String[] déplacementGuest(String[] Echiquier,int coordoner,String[] color){
+        String[] déplacement = new String[4];
+        int cpt=0,premier=0;
+        //test tout les endroi ou peux marcher un pion et ou il peux manger
+        coordoner -=7;
+        for (int i=0;i<3;i++){
+            if (i == 0 || i==2){
+                if(!Echiquier[coordoner].equals("") && color[coordoner].equals("B")){
+                    déplacement[cpt] = "A:"+coordoner;
+                    cpt++;
+                }else{
+                    déplacement[cpt] = "R:"+coordoner;
+                    cpt++;
+                }
+            }else{
+                if (Echiquier[coordoner].equals("")){
+                    déplacement[cpt] = "D:"+coordoner;
+                    cpt++;
+                    premier=1;
+                }else{
+                    déplacement[cpt] = "O:"+coordoner;
+                    cpt++;
+                    premier=0;
+                }
+            }
+            coordoner--;
+        }
+        coordoner -=6;
+        if(Echiquier[coordoner].equals("")&& premier == 1){
+            déplacement[cpt] = "D:"+coordoner;
+        }else{
+            déplacement[cpt] = "O:"+coordoner;
+        }
+
+        cpt=0;
+        return déplacement;
+    }
+}
