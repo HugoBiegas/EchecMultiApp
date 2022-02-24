a1a437f2263e7751493320f0d8fa1ad9f767fcab
Hugo <78080239+mordeur@users.noreply.github.com>
2022-02-24 19:33:56 +0100

    codage de la roi2.0

--- app/src/main/java/com/echec/echecmulti/Roi.java
+++ app/src/main/java/com/echec/echecmulti/Roi.java
@@ -1,5 +1,7 @@
 package com.echec.echecmulti;
 
+import android.widget.EditText;
+
 import java.util.ArrayList;
 
 public class Roi {
@@ -7,13 +9,15 @@ public class Roi {
         ArrayList<String> deplacement = new ArrayList<>();
         int[] borderD = new int[]{ 7, 15, 23, 31, 39, 47, 55, 63};
         int[] borderG = new int[]{0, 8, 16, 24, 32, 40, 48, 56};
-        int ecart,bor=0;
+        int ecart,bor=0,poseR,fin=0;
+
         for (int i=0;i<8;i++){
             if (borderD[i]==coordoner)
                 bor=1;
             else if (borderG[i]==coordoner)
                 bor=2;
         }
+
 
         //pour le haut
         if (bor==1){
@@ -24,37 +28,35 @@ public class Roi {
                         deplacement.add("D:"+ecart);
                     }else if (color[ecart].equals("N")){
                         deplacement.add("A:"+ecart);
-                        i=2;
                     }
                 }
                 ecart++;
             }
         }else if (bor==2){
-            ecart = coordoner +7;
-            for (int i=0;i<2;i++){
-                if (ecart<63 && ecart>0){
-                    if (Echiquier[ecart].equals("")){
-                        deplacement.add("D:"+ecart);
-                    }else if (color[ecart].equals("N")){
-                        deplacement.add("A:"+ecart);
-                        i=2;
+                ecart = coordoner +7;
+                for (int i=0;i<2;i++){
+                    if (ecart<63 && ecart>0){
+                        if (Echiquier[ecart].equals("")){
+                            deplacement.add("D:"+ecart);
+                        }else if (color[ecart].equals("N")){
+                            deplacement.add("A:"+ecart);
+                        }
                     }
+                    ecart++;
                 }
-                ecart++;
-            }
         }else{
-            ecart = coordoner +7;
-            for (int i=0;i<3;i++){
-                if (ecart<63 && ecart>0){
-                    if (Echiquier[ecart].equals("")){
-                        deplacement.add("D:"+ecart);
-                    }else if (color[ecart].equals("N")){
-                        deplacement.add("A:"+ecart);
-                        i=2;
+                ecart = coordoner +7;
+                for (int i=0;i<3;i++){
+                    if (ecart<63 && ecart>0){
+                        if (Echiquier[ecart].equals("")){
+                            deplacement.add("D:"+ecart);
+                        }else if (color[ecart].equals("N")){
+                            deplacement.add("A:"+ecart);
+                        }
                     }
+                    ecart++;
                 }
-                ecart++;
-            }
+
         }
 
         //pour le bat
@@ -66,7 +68,6 @@ public class Roi {
                         deplacement.add("D:"+ecart);
                     }else if (color[ecart].equals("N")){
                         deplacement.add("A:"+ecart);
-                        i=2;
                     }
                 }
                 ecart--;
@@ -79,7 +80,6 @@ public class Roi {
                         deplacement.add("D:"+ecart);
                     }else if (color[ecart].equals("N")){
                         deplacement.add("A:"+ecart);
-                        i=2;
                     }
                 }
                 ecart--;
@@ -92,7 +92,6 @@ public class Roi {
                         deplacement.add("D:"+ecart);
                     }else if (color[ecart].equals("N")){
                         deplacement.add("A:"+ecart);
-                        i=2;
                     }
                 }
                 ecart--;
@@ -144,7 +143,6 @@ public class Roi {
                         deplacement.add("D:"+ecart);
                     }else if (color[ecart].equals("B")){
                         deplacement.add("A:"+ecart);
-                        i=2;
                     }
                 }
                 ecart++;
@@ -157,7 +155,6 @@ public class Roi {
                         deplacement.add("D:"+ecart);
                     }else if (color[ecart].equals("B")){
                         deplacement.add("A:"+ecart);
-                        i=2;
                     }
                 }
                 ecart++;
@@ -170,7 +167,6 @@ public class Roi {
                         deplacement.add("D:"+ecart);
                     }else if (color[ecart].equals("B")){
                         deplacement.add("A:"+ecart);
-                        i=2;
                     }
                 }
                 ecart++;
@@ -186,7 +182,6 @@ public class Roi {
                         deplacement.add("D:"+ecart);
                     }else if (color[ecart].equals("B")){
                         deplacement.add("A:"+ecart);
-                        i=2;
                     }
                 }
                 ecart--;
@@ -199,7 +194,6 @@ public class Roi {
                         deplacement.add("D:"+ecart);
                     }else if (color[ecart].equals("B")){
                         deplacement.add("A:"+ecart);
-                        i=2;
                     }
                 }
                 ecart--;
@@ -212,7 +206,6 @@ public class Roi {
                         deplacement.add("D:"+ecart);
                     }else if (color[ecart].equals("B")){
                         deplacement.add("A:"+ecart);
-                        i=2;
                     }
                 }
                 ecart--;
